55625e23c0adc96d8db5923dff3a560b0d087b03
Vivek Gohil <59043694+vivek-gohil@users.noreply.github.com>
2026-03-25 10:37:52 +0530

    Transaction Service

---
+++ Misc/Spring Microservices/03_Transaction_Service/src/main/java/com/msedcl/main/transaction/actuator/TransactionMetricsEndpoint.java
@@ -0,0 +1,28 @@
+
+package com.msedcl.main.transaction.actuator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+import com.msedcl.main.transaction.repository.TransactionRepository;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+@Component
+@Endpoint(id = "transaction-metrics")
+public class TransactionMetricsEndpoint {
+
+	@Autowired
+	private TransactionRepository repository;
+
+	@ReadOperation
+	public Map<String, Object> metrics() {
+
+		long todayTransactions = repository.countByTransactionDate(LocalDate.now());
+
+		return Map.of("todayTransactions", todayTransactions);
+	}
+}
